36a776055f4f52e68b5de4d01ada59ac11fd27e1
Bandarban <Bandarban@users.noreply.github.com>
2018-02-14 18:19:58 +0500

    Test

---
+++ src/lab_1/HelloThere.java
@@ -0,0 +1,11 @@
+package lab_1;
+
+/**
+ * Created by Bandar on 14.02.2018.
+ */
+public class HelloThere {
+        public static void main(String args []) {
+
+            System.out.print("Hello World!");
+        }
+    }
